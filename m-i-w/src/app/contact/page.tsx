7bd3a900300c430dbb42916b3eb0b5994b00e507
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-09-16 22:13:50 +0300

    Made Contact as a separate module.

--- m-i-w/src/app/contact/page.tsx
+++ m-i-w/src/app/contact/page.tsx
@@ -1,7 +1,13 @@
-export default function Contactt() {
+import Contacts from "@/components/contacts/Contacts";
+
+export default function ContactPage() {
     return (
         <section>
-            <h1 className="text-2xl">Contact</h1>
+            <h1 className="text-2xl text-center py-4">Contacts</h1>
+            <Contacts
+                styleUl='pb-2'
+                styleLink='flex items-center justify-center p-4'
+            />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, aliquid! Dolorum, sed atque! Similique reiciendis eum dicta dolore, praesentium delectus accusantium facere consectetur ad repudiandae ullam dolores magnam dolorum asperiores?</p>
 
             <h2 className="text-xl">Form</h2>
