ff0a645645b1c810be12bc1bf8d5daad4a987526
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-09-24 15:08:51 +0300

    Data from separate *.md files are already loading

--- m-i-w/src/app/posts/[id]/page.tsx
+++ m-i-w/src/app/posts/[id]/page.tsx
@@ -1,34 +1,51 @@
-import { getAllPostIds, getPostData } from "@/lib/posts"
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import ReactMarkdown from 'react-markdown';
+
+import { getAllPostIds, getPostData } from "@/lib/posts";
+
+interface PostData {
+    content: string;
+    frontmatter: {
+        title: string;
+    };
+}
 
 export function generateStaticParams() {
     const res = getAllPostIds();
-    // return [{ id: '1' }, { id: '2' }, { id: '3' }];
     return res;
 }
 
-export default function Page({ params }: { params: { id: string } }) {
+export default function Post({ params }: { params: { id: string } }) {
     const { id } = params;
-
-    async function fetchData() {
-        try {
-            const postData = (await getPostData(id));
-            console.log('inside fetchData',postData); // Тут ви маєте доступ до отриманих даних
-            return postData.contentHtml;
-        } catch (error) {
-            console.error(error); // Обробка помилки, якщо є
-        }
-    }
-
-    let postData= fetchData();
-
-    console.log(`post data ${postData}`);
-
+    const postFilePath = path.join(process.cwd(), 'posts', `${id}.md`);
+    // console.log(`postFilePath ${postFilePath}`);
+    const fileContents = fs.readFileSync(postFilePath, 'utf-8');
+    const { content, data } = matter(fileContents);
+    // console.log(`content ${content}`);
     return (
-        <>
-            <div>
-                <h1 className='text-center text-2xl'>Pages id {id}</h1>
-                {postData}
+        <div>
+            <h1 className='text-center text-2xl'>{data.title}</h1>
+            <div className='flex justify-between'>
+                <div>{data.place}</div>
+                <data>{data.date}</data>
             </div>
-        </>
-    )
+            <ReactMarkdown>{content}</ReactMarkdown>
+        </div>
+    );
 }
+
+// export function getStaticProps({ params }: any) {
+//     const postFilePath = path.join(process.cwd(), 'posts', `${params.slug}.md`);
+//     console.log(`postFilePath ${postFilePath}`);
+//     const fileContents = fs.readFileSync(postFilePath, 'utf-8');
+//     const { content, data } = matter(fileContents);
+//     console.log(`content ${content}`);
+//     return {
+//         props: {
+//             content,
+//             frontmatter: data,
+//         },
+//     };
+// }
