36ea9ac234bcd87fdcde8987a46b05e0e40f4cdc
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-10-02 13:50:37 +0300

    Added generate metadata function to [id] page

--- m-i-w/src/app/posts/[id]/page.tsx
+++ m-i-w/src/app/posts/[id]/page.tsx
@@ -3,9 +3,8 @@ import path from 'path';
 import matter from 'gray-matter';
 import ReactMarkdown from 'react-markdown';
 import Date from '@/components/date';
-// import Image from 'next/image';
 
-import { getAllPostIds} from "@/lib/posts";
+import { getAllPostIds } from "@/lib/posts";
 
 interface PostData {
     content: string;
@@ -14,12 +13,43 @@ interface PostData {
     };
 }
 
+
+import { Metadata, ResolvingMetadata } from 'next'
+
+type Props = {
+    params: { id: string }
+    searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export async function generateMetadata(
+    { params, searchParams }: Props,
+    parent: ResolvingMetadata
+): Promise<Metadata> {
+    
+    const { id } = params;
+    const postFilePath = path.join(process.cwd(), 'posts', `${id}.md`);
+    const fileContents = fs.readFileSync(postFilePath, 'utf-8');
+    const { data } = matter(fileContents);
+
+    const previousImages = (await parent).openGraph?.images ?? []
+
+    return {
+        title: data.title,
+        openGraph: {
+            images: [data.titleImage, ...previousImages],
+            title: data.title,
+            type: 'website',
+        },
+    }
+}
+
+
 export function generateStaticParams() {
     const res = getAllPostIds();
     return res;
 }
 
-export default function Post({ params }: { params: { id: string } }) {
+export default function Post({ params, searchParams }: Props) {
     const { id } = params;
     const postFilePath = path.join(process.cwd(), 'posts', `${id}.md`);
     // console.log(`postFilePath ${postFilePath}`);
