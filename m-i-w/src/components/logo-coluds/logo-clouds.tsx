4581ad6a8bad45e07162942d226b25570dfe3c27
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-10-06 21:16:13 +0300

    refactor: main

--- m-i-w/src/components/logo-coluds/logo-clouds.tsx
+++ m-i-w/src/components/logo-coluds/logo-clouds.tsx
@@ -5,9 +5,9 @@ export default function LogoClouds() {
     return (
         <div className="mx-auto md:px-6 text-center">
             <h2 className="mb-16 text-3xl font-bold">Tech stack</h2>
-            <ul className="grid justify-items-center gap-6 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5">
+            <ul className="grid justify-items-center gap-6 sm:grid-cols-2 m:grid-cols-4 lg:grid-cols-5">
                 {logos.map((logo) => (
-                    <li key={logo.id} className="list-none w-1/2 h-1/2 md:w-50 md:h-50 lg:mb-0 hover:bg-teal-200 dark:hover:bg-teal-700">
+                    <li key={logo.id} className="list-none w-1/2 h-1/2">
                         <Image
                             src={logo.src}
                             alt={logo.alt}
