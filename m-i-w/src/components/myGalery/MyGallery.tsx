8869ea3ad34cb11b4df1377792652389efcec6df
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-10-07 23:02:58 +0300

    refactor: myGalary
    with Next Image component with sizes

--- m-i-w/src/components/myGalery/MyGallery.tsx
+++ m-i-w/src/components/myGalery/MyGallery.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { images } from './myGalleryData';
+import Image from 'next/image';
 
 export default function MyGallery() {
     return (
@@ -7,9 +8,12 @@ export default function MyGallery() {
             <div className="flex flex-wrap">
                 {images.map((image) => (
                     <div key={image.id} className="flex w-full md:w-1/2 lg:w-1/3 flex-wrap p-1 md:p-2">
-                        <img
+                        <Image
                             src={image.src}
                             alt={image.alt}
+                            width={image.imgWidth}
+                            height={image.imgHeight}
+                            sizes="(max-width: 768px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 34vw"
                             className="block h-full w-full rounded-lg object-cover object-center"
                         />
                     </div>
--- m-i-w/src/components/myGalery/myGalleryData.tsx
+++ m-i-w/src/components/myGalery/myGalleryData.tsx
@@ -3,15 +3,21 @@ export const images = [
         id: 1,
         alt: "Принесли шеврони",
         src: "/images/posts-img/main/IMG_20220516_201551.jpg",
+        imgWidth: 3472,
+        imgHeight: 4624,
     },
     {
         id: 2,
         alt: "Селфі на фоні броніка каски та автоматика",
         src: "/images/posts-img/main/IMG_20220618_121047.jpg",
+        imgWidth: 4623,
+        imgHeight: 3460,
     },
     {
         id: 3,
         alt: "Спецназ ТРО ;-)",
         src: "/images/posts-img/main/IMG_20220721_185028.jpg",
+        imgWidth: 2592,
+        imgHeight: 1936,
     },
 ]
