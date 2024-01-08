bce264b454b089682368aab6a79f41d2f287937f
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2024-01-08 15:51:39 +0200

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,14 +9,12 @@ export const metadata: Metadata = {
 export default function ContactPage() {
     return (
         <section>
-            <h1 className="text-center py-4">Contact me</h1>
+            <h1 className="text-center py-4">Contact us page</h1>
             <Contacts
                 styleUl='pb-2'
                 styleLink='flex items-center justify-center p-4'
             />
             <hr />
-            {/* <h2 className="text-xl text-center">Form</h2> */}
-            {/* <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis aliquam provident tenetur? Similique eos delectus assumenda sequi ipsa incidunt cumque officia deserunt. Perspiciatis officia soluta dignissimos sapiente voluptates, magnam hic.</p> */}
             <ContactForm/>
         </section>
     )
